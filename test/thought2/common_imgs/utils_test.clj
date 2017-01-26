5e47983c33656cdb4137e242ebfea5dcf8d73481
Thought2 <me@thought2.de>
2017-01-26 17:06:55 +0100

    init

--- test/thought2/common_imgs/utils_test.clj
+++
@@ -1,9 +0,0 @@
-(ns thought2.common-imgs-test
-  (:require [clojure.test :refer :all]
-            [thought2.common-imgs.utils :as utils]))
-
-
-(deftest lt-both?-test
-  (is (utils/lt-both? [20 20] [30 30]))
-  (is (not (utils/lt-both? [40 20] [30 30])))  
-  (is (not (utils/lt-both? [40 40] [30 30]))))
